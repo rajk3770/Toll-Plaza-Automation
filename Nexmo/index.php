1f81230575b9850bb963be61886803e1e05717a8
Raj Ashish Kothari <30768355+rajk3770@users.noreply.github.com>
2018-05-04 00:41:58 +0530

    Update index.php

--- Nexmo/index.php
+++ Nexmo/index.php
@@ -7,9 +7,9 @@
     class SMS{
 	// Step 1: Declare new NexmoMessage.
 	function sendSMS(){
-        $nexmo_sms = new NexmoMessage('dec8a214', '04231f6bc7589967');
+        $nexmo_sms = new NexmoMessage('YOUR_ID', 'YOUR_APP_KEY');
 	// Step 2: Use sendText( $to, $from, $message ) method to send a message. 
-	$info = $nexmo_sms->sendText( '919022058065', 'MyApp', 'Hello!' );
+	$info = $nexmo_sms->sendText( 'PHONE_NUMBER', 'MyApp', 'Hello!' );
 	// Step 3: Display an overview of the message
 	//echo $nexmo_sms->displayOverview($info);
 	// Done!}}
